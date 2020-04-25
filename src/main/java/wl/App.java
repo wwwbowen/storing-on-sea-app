415faa127e91af882ce673614c50c994fdcd2749
wwwbowen <1476443537@qq.com>
2020-04-25 11:13:22 +0800

    database test

---
+++ src/main/java/wl/App.java
@@ -0,0 +1,14 @@
+package wl;
+
+/**
+ * Demo
+ */
+public class App {
+
+    public static void main(final String[] args) {
+        final Login login = new Login();
+        login.setVisible(true);
+        
+    
+    }
+}
