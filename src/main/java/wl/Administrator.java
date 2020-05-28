6f76f0edd1cd811bad08f7993043189683472ec1
wwwbowen <1476443537@qq.com>
2020-05-28 22:52:41 +0800

    master

--- src/main/java/wl/Administrator.java
+++ src/main/java/wl/Administrator.java
@@ -44,6 +44,13 @@ public class Administrator extends JFrame {
         this.setLocation(375, 100);
         this.setJMenuBar(bar);
 
+        setVisible(true);
+        ((JComponent) getContentPane()).setOpaque(false); //将框架强转为容器          
+        final ImageIcon img = new ImageIcon("src/main/images/注册栏背景.gif"); // 传入背景图片路径
+        final JLabel background = new JLabel(img);// 将图片放进标签里
+        getLayeredPane().add(background, new Integer(Integer.MIN_VALUE));// 将标签放进容器里
+        background.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());// 设置标签的大小
+
         menu_file.setFont(kaiFont);
         bar.add(menu_file);
 
